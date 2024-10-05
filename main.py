ef3cb565a144b30f02f1e06ab4b5627f26f778ff
Ilia Trofimov <ilia.trofimov2001@icloud.com>
2024-10-05 21:36:50 +0300

    Second commit

--- main.py
+++ main.py
@@ -5,35 +5,50 @@ import matplotlib.animation as animation
 import matplotlib.widgets as widgets
 
 import numpy as np
+from matplotlib.gridspec import GridSpec
 from scipy.integrate import solve_ivp
 
+import models
 import utils
 from utils import logger
 from handlers import *
 
 
-alpha = 1.0
-epsilon = 1.0
-gamma = 1.0
-mu = 1.0
-dt = 0.01
-
-
-def bazykin_model_a(t, *y):
-    a = random.random()
-    return (y[0] - y[0]*y[1]/(1 + a*y[0]) - epsilon*y[0]**2,
-            -gamma*y[1] + y[0]*y[1]/(1 + a*y[0]) - mu*y[1]**2)
-
-
-def bazykin_solution_a(x0: float, y0: float, t: float):
-    return solve_ivp(bazykin_model_a, (0, t), (x0, y0), first_step=dt).y
+model = models.BazykinModelA()
+sliders: list[tuple[plt.Axes, widgets.Slider]] = []
 
 
 def draw_trace(x0, y0, axes: plt.Axes, fig: plt.Figure):
     logger.debug(f'draw_trace: {x0:.3f} {y0:.3f}')
-    trace = bazykin_solution_a(x0, y0, 5.0)
-    axes.plot(trace[:, 0], trace[:, 1], label=f"$x_0={x0:.3f}$, $y_0={y0:.3f}$")
+    trace = model.solve(x0, y0, 10, dt=0.1)
+    axes.plot(trace[0], trace[1], label=f"$x_0={x0:.3f}$, $y_0={y0:.3f}$")
     fig.show()
+
+
+def clear_axes(*axes: plt.Axes):
+    for ax in axes:
+        ax.set_xticks([])
+        ax.set_yticks([])
+        ax.tick_params(left=False, bottom=False, labelbottom=False)
+        ax.spines[:].set_visible(False)
+
+
+def init_sliders(heatmap_updater: HeatmapUpdateHandler):
+    for ax, slider in sliders:
+        ax.clear()
+    sliders.clear()
+
+    def on_slider_update(n, v):
+        last_v = model.get_param(n)
+        model.set_param(n, v)
+        if abs(last_v - v) > 0.05:
+            heatmap_updater.init()
+
+    for i, (p_name, (p_value, p_min, p_max)) in enumerate(model.enum_parameters().items()):
+        ax = plt.axes((0.03, 0.95 - 0.1*(i+1), 0.20, 0.08))
+        slider = widgets.Slider(ax=ax, label=p_name, valmin=p_min, valmax=p_max, valinit=p_value)
+        sliders.append((ax, slider))
+        slider.on_changed(lambda v: on_slider_update(p_name, v))
 
 
 if __name__ == "__main__":
@@ -41,15 +56,28 @@ if __name__ == "__main__":
     logger.info('Starting execution...')
 
     fig, ax_main = plt.subplots()
-    ax_next = fig.add_axes([0.81, 0.05, 0.1, 0.075])
-    btn_update = widgets.Button(ax_next, 'Update')
+    ax_main.grid()
+    ax_main.set_xlabel('Prey, $x$')
+    ax_main.set_ylabel('Predators, $y$')
 
-    heatmap_update = HeatmapUpdateHandler(fig, ax_main, bazykin_model_a,
+    fig.subplots_adjust(left=0.32, right=0.99, top=0.98, bottom=0.2)
+    fig.canvas.manager.set_window_title('Bazykin population model')
+
+    ax_corner = plt.axes((0.03, 0.03, 0.20, 0.10))
+    ax_bottom = plt.axes((0.7, 0.05, 0.25, 0.075))
+
+    clear_axes(ax_corner, ax_bottom)
+
+    heatmap_update = HeatmapUpdateHandler(fig, ax_main, model,
                                           [0, 5], [0, 5])
     heatmap_update.init()
 
     clicker = OnSetPointHandler(fig, ax_main, action=draw_trace)
+
+    btn_update = widgets.Button(ax_corner, 'Update', )
     btn_update.on_clicked(heatmap_update)
+
+    init_sliders(heatmap_update)
 
     plt.show()
 
--- models.py
+++ models.py
@@ -1,3 +1,85 @@
 import numpy as np
+from scipy.integrate import solve_ivp
 
 
+class BaseOdeModel:
+    def __call__(self, t: float, yy: np.ndarray | list[float] | tuple[float]):
+            raise NotImplemented()
+
+    def solve(self, x0: float, y0: float, t: float, dt: float = 0.1):
+        return solve_ivp(self.__call__, (0, t), (x0, y0), first_step=dt).y
+
+    def validate_params(self):
+        pass
+
+    def latex_str(self):
+        return "$x' = f(t, x, y)$\n$y' = g(t, x, y)$"
+
+    def enum_parameters(self):
+        return {}
+
+    def set_param(self, p_name: str, value):
+        pass
+
+
+class BazykinModelA(BaseOdeModel):
+    def __init__(self, alpha: float = 1, gamma: float = 1, eps: float = 1, mu: float = 1):
+        self.alpha = alpha
+        self.gamma = gamma
+        self.eps = eps
+        self.mu = mu
+
+    def __call__(self, t: float, yy: np.ndarray | list[float] | tuple[float]):
+        return (yy[0] - yy[0]*yy[1] / (1 + self.alpha*yy[0]) - self.eps*yy[0]**2,
+                -self.gamma*yy[1] + yy[0]*yy[1] / (1 + self.alpha*yy[0]) - self.mu*yy[1]**2)
+
+    def validate_params(self):
+        if self.alpha < 0: self.alpha = 0.0
+        elif self.alpha > 10: self.alpha = 10.0
+
+        if self.gamma < 0: self.gamma = 0.0
+        elif self.gamma > 10: self.gamma = 10.0
+
+        if self.eps < 0: self.eps = 0.0
+        elif self.eps > 10: self.eps = 10.0
+
+        if self.mu < 0: self.mu = 0.0
+        elif self.mu > 10: self.mu = 10.0
+
+    def __str__(self):
+        return (f"x' = x - xy/(1 + {self.eps:.3f}x) - {self.eps:.3f}x^2\n"
+                f"y' = {-self.gamma:.3f}y + xy/(1 + {self.eps:.3f}x) - {self.mu:.3f}y^2")
+
+    def latex_str(self):
+        return (f"$x' = x - \\frac{{xy}}{{1 + {self.eps:.3f}x}} - {self.eps:.3f}x^2$\n"
+                f"$y' = {-self.gamma:.3f}y + \\frac{{xy}}{{1 + {self.eps:.3f}x}} - {self.mu:.3f}y^2$")
+
+    def enum_parameters(self):
+        return {
+            "α": (self.alpha, 0, 10),
+            "γ": (self.gamma, 0, 10),
+            "μ": (self.mu, 0, 10),
+            "ε": (self.eps, 0, 10)
+        }
+
+    def set_param(self, p_name: str, value: float):
+        match p_name:
+            case "α":
+                self.alpha = value
+            case "γ":
+                self.gamma = value
+            case "μ":
+                self.mu = value
+            case "ε":
+                self.eps = value
+            case _:
+                raise KeyError(f"Unknown model parameter '{p_name}'")
+
+    def get_param(self, p_name: str):
+        match p_name:
+            case "α": return self.alpha
+            case "γ": return self.gamma
+            case "μ": return self.mu
+            case "ε": return self.eps
+            case _:
+                raise KeyError(f"Unknown model parameter '{p_name}'")
